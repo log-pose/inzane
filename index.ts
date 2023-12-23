64dd3735c554d1e11ecf2d8e36856cab73a69083
Sebin CM <sebinsebzz2002@gmail.com>
2023-12-23 22:20:00 +0530

    setup express

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+import logger from "conf/logger";
+import app from "conf/server";
+
+const PORT = process.env.INZANE_PORT || 7450;
+
+app.listen(PORT, () => {
+  logger.info(`Nakama server listening on port ${PORT}`);
+});
